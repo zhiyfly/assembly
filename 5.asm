d99ebaae1917f268c007eae27baf4cc24a9fb6a2
zhiyfly <zhiyfly@gmail.com>
2015-05-09 17:18:42 +0800

    modify 4.asm, add 5.asm

--- 5.asm
+++ 5.asm
@@ -1,6 +1,6 @@
-assume cs:codeseg, ds:dataseg, ss:stackseg
+assume cs:codeseg ds:dataseg ss:stackseg
 dataseg segment
-	dw 0012H,0345H,0678H,09abH,0cdeH,0f01H,0234H,0567H
+	dw 
 dataseg ends	
 stackseg segment
 	dw 0,0,0,0,0,0,0,0
@@ -14,14 +14,16 @@ codeseg segment
 		mov sp, 16
 		mov cx, 8
 		mov bx, 0
-		s:push [bx]
+		l1:
+		   push [bx]
 		   add bx,2
-		   loop s
+		   loop l1
+
 		mov bx, 0
 		mov cx, 8
-		s0:pop [bx]
+		l2:pop [bx]
 		   add bx, 2
-		   loop s0
+		   loop l2
 		mov ax,4c00H
 		int 21h
 codeseg ends
